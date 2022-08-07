a46905cb52ac13841579f6d49c328524bf0052f1
daphnelink@gmail.com <daphnelink@gmail.com>
2022-08-06 18:45:17 -0700

    scraper created using jsoup
    jsoup is now scraping to pull list of games

--- gameschariot/src/main/java/com/gameschariot/gameschariot/GamesChariotApplication.java
+++ gameschariot/src/main/java/com/gameschariot/gameschariot/GamesChariotApplication.java
@@ -1,5 +1,9 @@
 package com.gameschariot.gameschariot;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
@@ -9,7 +13,13 @@ public class GamesChariotApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(GamesChariotApplication.class, args);
+		// scraper
+		Document doc = Jsoup.connect("https://help.netflix.com/en/node/121442").get();
+		Elements listOfGames = doc.select("div[class=\"tab level-1 open\"] > div[class=\"tab-content\"] > div[class=\"c-wrapper\"] > div > ul > li > p > a[href]");
 
+		for(Element game : listOfGames) {
+			System.out.println(game.text());
+		}
 	}
 }
 
--- gameschariot/src/main/java/com/gameschariot/gameschariot/GreetingController.java
+++ gameschariot/src/main/java/com/gameschariot/gameschariot/GreetingController.java
@@ -1,7 +1,14 @@
 package com.gameschariot.gameschariot;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,5 +24,23 @@ public class GreetingController {
     @GetMapping("/")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/games-list")
+    public Greeting gamesList() throws IOException {
+        // scraper
+        Document doc = Jsoup.connect("https://help.netflix.com/en/node/121442").get();
+        Elements listOfGames = doc.select(".c-wrapper div li a");
+        System.out.println(doc.select(".c-wrapper div li a"));
+
+        List<String> gameTitles = new ArrayList<>();
+        for(Element game : listOfGames) {
+            gameTitles.add(game.ownText());
+        }
+        // remove first 2 items and last item from list
+        gameTitles.remove(0);
+        gameTitles.remove(0);
+        gameTitles.remove(gameTitles.size()-1);
+        return new Greeting(counter.incrementAndGet(), gameTitles.toString());
     }
 }
